6c066dddfe9d630f53f82946e15631d54b6b42b4
xuanispin <1844188196@qq.com>
2019-10-21 15:35:37 +0800

    jswork

--- work7slide/utils.js
+++ work7slide/utils.js
@@ -2,10 +2,15 @@
 function bubbleSort(str){
     //把字符串分成数组
     let arr = str.split(',')
+    //声明排序步骤记录数组sortLog数组的数据用于动画演示
+    let sortLog = []
     //比较轮数，数组有多少个数字，就比较数组长度-1轮
     for (let i = 0;i < arr.length - 1; i++){
         //从第一个开始比较相邻的两个数值，
         for (let j = 0; j < arr.length - 1; j++) {
+            //把比较前的数组状态和定位指针保存到sortLog中，为保持演示效果，加多两行。
+            sortLog.push([arr.concat(),[j, j + 1]])
+            
             //比较相邻的两个元素，如果左面的大，那么就把大的交换到右边
             if (arr[j] > arr[j + 1]) {
                 //使用解构赋值的方法，把两个元素交换位置
