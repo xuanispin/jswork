e1fa956391643f1baeb2f56b0f21475ee53ec776
apple <apple@jd505-18.local>
2019-12-09 15:43:02 +0800

    jswork

---
+++ work15/ShopCart.js
@@ -0,0 +1,8 @@
+(function(window){
+    /**
+     * ShopCart 购物车
+     * @param{String} prefix 前端
+     *@param{Array} defCart 初始数据 [{name:'',price:1,num:1}]
+     */
+    let ShopCart = function(prefix,defCart)
+})
