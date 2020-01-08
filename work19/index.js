1ab8d022dc1bd2e7e9e5f6e1b3499485272227ba
xuanispin <1844188196@qq.com>
2020-01-08 19:53:34 +0800

    jswork

--- work19/index.js
+++ work19/index.js
@@ -1,6 +1,6 @@
 let ws
 function connect(){
-    let server = document.getElementById('server').Value
+    let server = document.getElementById('server').value
     ws = new WebSocket(server);
     ws.onopen = function(){
         document.getElementById('conn').disabled='disable';
