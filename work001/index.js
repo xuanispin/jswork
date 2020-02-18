6a8601999c3b9d18ac65ef5b4a88f3db579e8ce4
xuanispin <1844188196@qq.com>
2020-02-18 20:20:34 +0800

    work001

--- work001/index.js
+++ work001/index.js
@@ -1,3 +1,67 @@
-function heart(){
-    document.getElementById('hurt').innerHTML = 'Forward,boy,do not think more';
+function changePicture1(){
+    document.getElementById("image1").src="images/1.png";
 }
+
+function changePicture2(){
+    document.getElementById("image1").src="images/2.png";
+}
+
+                                        // 第一部分，改变图片。
+
+function changeText1(){
+    document.getElementById("change1").innerHTML = "暗影疾行";
+}
+
+function changeText2(){
+    document.getElementById("change1").innerHTML = "一代又一代圣堂武士，选择了黑暗圣堂之路。";
+}
+
+                                        // 第二部分，改变文字。
+
+function blockTime1(){
+    document.getElementById("time1").innerHTML = Date();
+}
+
+                                        // 第三部分，显示时间。
+
+function cText1(){
+    document.getElementById('textbig').style.fontSize = '50px';
+}
+
+function cText2(){
+    document.getElementById('textbig').style.fontSize = '30px';
+}
+
+function cText3(){
+    document.getElementById('textbig').style.display = 'none';
+}
+
+function cText4(){
+    document.getElementById('textbig').style.display = 'block';
+}
+
+                                        // 第四部分，改变文字2。
+
+    document.getElementById('save').innerHTML = "刺客信条";
+
+                                        // 第五部分，显示文字。
+
+function myFunction(){
+    document.getElementById('assassin').innerHTML = 'Creed';
+}
+
+function myRoad(){
+    document.getElementById('road').innerHTML = 'Forward,do not look back';
+}
+
+function heart(){
+    document.getElementById('hurt').innerHTML = 'Forward,do not think more';
+}
+
+                                        // 第六部分，改变文字3。
+
+function Color(abc){
+    document.body.style.backgroundColor = abc;
+}
+
+                                        // 第七部分，改变背景颜色。
